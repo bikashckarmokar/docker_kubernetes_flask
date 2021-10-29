93f47c1170bc4409603bf64249c4e54d7fd2d331
Bikash Karmokar <chandrakarmokar.bikash@gmail.com>
2021-10-29 18:52:18 +0200

    added template to show the input as a goal in the flask app.
    Signed-off-by: Bikash Karmokar <chandrakarmokar.bikash@gmail.com>

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h2> Hi There </h2>'
+    goal = 'Learning'
+    return render_template('home.html', goal=goal)
 
+
+@app.route('/store-goal', methods=['POST'])
+def store_goal():
+    goal = request.form['goal']
+    return render_template('home.html', goal=goal)
 
 if __name__=='__main__':
     app.run()
