df880b4d449bfd19f9f3e25a297377266db3965a
Bikash Karmokar <chandrakarmokar.bikash@gmail.com>
2021-11-16 12:29:28 +0100

    lecture 31- random number generation
    Signed-off-by: Bikash Karmokar <chandrakarmokar.bikash@gmail.com>

---
+++ rng.py
@@ -0,0 +1,11 @@
+from random import randint
+
+min_number = int(input("Enter the minimum number: "))
+max_number = int(input("Enter the maximum number: "))
+
+if (min_number < max_number):
+    print("Invalid input - shutting down..")
+
+else:
+    rnd_number = randint(min_number, max_number)
+    print("The random number is: ", rnd_number)
